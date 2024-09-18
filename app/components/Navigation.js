8738412b4160339fd57b682e16d567e36f587d3f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:34:56 +0300

    add content to nav

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,11 +1,19 @@
 import Link from 'next/link'
+import { PrayingHands } from 'lucide-react'
 
 export default function Navigation() {
-  return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      {/* Add more navigation links */}
-    </nav>
-  )
+    return (
+        <nav className="bg-purple-700 text-white p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href="/" className="flex items-center space-x-2">
+                    <PrayingHands size={24} />
+                    <span className="font-bold text-xl">PrayerConnect</span>
+                </Link>
+                <div className="space-x-4">
+                    <Link href="/" className="hover:text-purple-200">Home</Link>
+                    <Link href="/about" className="hover:text-purple-200">About</Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
