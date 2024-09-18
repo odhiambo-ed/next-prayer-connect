a10ae8fba6779f90641b60e959693c161f8664e0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:42:07 +0300

    add share

--- app/components/ShareButtons.js
+++ app/components/ShareButtons.js
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button"
+import { Facebook, Twitter, Instagram } from 'lucide-react'
+
 export default function ShareButtons() {
-  return (
-    <div>
-      {/* Social media share buttons */}
-    </div>
-  )
+    return (
+        <div className="mt-8 text-center">
+            <h3 className="text-xl font-semibold mb-4">Share PrayerConnect</h3>
+            <div className="flex justify-center space-x-4">
+                <Button variant="outline" size="icon">
+                    <Facebook className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" size="icon">
+                    <Twitter className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" size="icon">
+                    <Instagram className="h-4 w-4" />
+                </Button>
+            </div>
+        </div>
+    )
 }
