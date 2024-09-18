a362cabf6ea01ca66f2fc113125dde3e1bcfdf58
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:24:05 +0300

    add nav component

---
+++ app/components/Navigation.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Navigation() {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      {/* Add more navigation links */}
+    </nav>
+  )
+}
