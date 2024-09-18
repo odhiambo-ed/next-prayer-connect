9d450f2abec45f7c3329cbbfe7ab7336696572bb
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:47:52 +0300

    import components home

--- app/page.js
+++ app/page.js
@@ -1,14 +1,22 @@
-import Hero from './components/Hero'
-import PrayerForm from './components/PrayerForm'
-import PrayerRequestList from './components/PrayerRequestList'
+import Navigation from '../components/Navigation'
+import AudioPlayer from '../components/AudioPlayer'
+import Hero from '../components/Hero'
+import PrayerRequestList from '../components/PrayerRequestList'
+import ShareButtons from '../components/ShareButtons'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <Navigation />
+      <AudioPlayer />
       <Hero />
-      <PrayerForm />
-      <PrayerRequestList />
-    </main>
+      <main className="flex-grow container mx-auto p-4 max-w-6xl">
+        <h2 className="text-3xl font-bold text-center my-8 text-purple-800">Recent Prayer Requests</h2>
+        <PrayerRequestList />
+        <ShareButtons />
+      </main>
+      <Footer />
+    </>
   )
 }
-
