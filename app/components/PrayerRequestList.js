cdf2238c9090c4e04531b9249a39bfd0e5270e1d
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:26:50 +0300

    add prayer list

---
+++ app/components/PrayerRequestList.js
@@ -0,0 +1,9 @@
+import PrayerRequestCard from './PrayerRequestCard'
+
+export default function PrayerRequestList() {
+  return (
+    <div>
+      {/* Map through prayer requests and render PrayerRequestCard */}
+    </div>
+  )
+}
