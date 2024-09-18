93e47e9ba327a7357b66e66d68efae27403f13b3
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:32:59 +0300

    import components to page

--- app/terms/page.js
+++ app/terms/page.js
@@ -1,8 +1,22 @@
-export default function Terms() {
-  return (
-    <div>
-      <h1>Terms of Service</h1>
-      <p>Our terms of service...</p>
-    </div>
-  )
+import Navigation from '../components/Navigation'
+import AudioPlayer from '../components/AudioPlayer'
+import Hero from '../components/Hero'
+import PrayerRequestList from '../components/PrayerRequestList'
+import ShareButtons from '../components/ShareButtons'
+import Footer from '../components/Footer'
+
+export default function Home() {
+    return (
+        <>
+            <Navigation />
+            <AudioPlayer />
+            <Hero />
+            <main className="flex-grow container mx-auto p-4 max-w-6xl">
+                <h2 className="text-3xl font-bold text-center my-8 text-purple-800">Recent Prayer Requests</h2>
+                <PrayerRequestList />
+                <ShareButtons />
+            </main>
+            <Footer />
+        </>
+    )
 }
