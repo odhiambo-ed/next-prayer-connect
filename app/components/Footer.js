8565d3f2bdda143454e43082097bcd9c51182314
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-18 22:43:47 +0300

    add footer content

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 export default function Footer() {
-  return (
-    <footer>
-      {/* Footer content */}
-    </footer>
-  )
+    return (
+        <footer className="bg-purple-800 text-white p-4 mt-8">
+            <div className="container mx-auto text-center">
+                <p>&copy; 2023 PrayerConnect. All rights reserved.</p>
+                <div className="mt-2 flex justify-center space-x-4">
+                    <Link href="/privacy" className="hover:text-purple-200">Privacy Policy</Link>
+                    <Link href="/terms" className="hover:text-purple-200">Terms of Service</Link>
+                </div>
+            </div>
+        </footer>
+    )
 }
